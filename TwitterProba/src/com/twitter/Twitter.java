faf4d5c1c1c6a0104a1aab260f6ef26446dc96fc
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-31 18:14:09 +0200

    Napravljene test metode za klasu Twitter, ostaje jos testVratiPorukeSveOk.

--- TwitterProba/src/com/twitter/Twitter.java
+++ TwitterProba/src/com/twitter/Twitter.java
@@ -36,9 +36,12 @@ public class Twitter {
 	 *            nova vrednost atributa poruka
 	 */
 	public void unesi(String korisnik, String poruka) {
+		if (korisnik == "" || poruka == "") {
+			throw new RuntimeException("Atributi korisnik i poruka ne smeju biti null vrednosti!");
+		}
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
-		tp.setKorisnik("korisnik");
+		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
@@ -59,8 +62,9 @@ public class Twitter {
 		if (tag == null || tag == "")
 			throw new RuntimeException("Morate uneti tag");
 		// Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
-		if (maxBroj <= 0)
+		if (maxBroj <= 0) {
 			maxBroj = 100;
+		}
 		// Pomocna promenljiva koja predstavlja brojac upisanih poruka
 		int brojac = 0;
 		// Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
@@ -73,7 +77,7 @@ public class Twitter {
 		for (int i = 0; i < poruke.size(); i++)
 			if (poruke.get(i).getPoruka().indexOf(tag) != -1)
 				if (brojac < maxBroj) {
-					rezultat[brojac + 1] = poruke.get(i);
+					rezultat[brojac] = poruke.get(i);
 					brojac++;
 				} else
 					break;
--- TwitterProba/test/com/twitter/TwitterTest.java
+++ TwitterProba/test/com/twitter/TwitterTest.java
@@ -46,9 +46,87 @@ public class TwitterTest {
 		assertEquals(poruka, t.vratiSvePoruke().getLast().getPoruka());
 	}
 
-	@Test
-	public void testVratiPoruke() {
-		fail("Not yet implemented");
+	/*
+	 * @Test (expected = java.lang.RuntimeException.class) public void
+	 * testUnesiNullKorisnik() { String korisnik = null; String poruka =
+	 * "Grmi FON!";
+	 * 
+	 * t.unesi(korisnik, poruka); assertEquals(korisnik,
+	 * t.vratiSvePoruke().getLast().getKorisnik()); assertEquals(poruka,
+	 * t.vratiSvePoruke().getLast().getPoruka()); }
+	 * 
+	 * @Test (expected = java.lang.RuntimeException.class) public void
+	 * testUnesiNullPoruka() { String korisnik = "Jovana"; String poruka = null;
+	 * 
+	 * t.unesi(korisnik, poruka); assertEquals(korisnik,
+	 * t.vratiSvePoruke().getLast().getKorisnik()); assertEquals(poruka,
+	 * t.vratiSvePoruke().getLast().getPoruka()); }
+	 */
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testUnesiNull() {
+		String korisnik = null;
+		String poruka = null;
+
+		t.unesi(korisnik, poruka);
 	}
 
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testUnesiPrazanString() {
+		String korisnik = "";
+		String poruka = "";
+
+		t.unesi(korisnik, poruka);
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testUnesiPredugaPoruka() {
+		String korisnik = "Jovana";
+		String poruka = "";
+
+		while (poruka.length() < 150) {
+			poruka += "j";
+		}
+
+		t.unesi(korisnik, poruka);
+	}
+
+	@Test
+	public void testVratiPorukeSveOk() {
+		
+		t.unesi("Jovana", "#cu");
+		
+		TwitterPoruka[] nizTviterPoruka = new TwitterPoruka[10];
+		
+		nizTviterPoruka[0] = new TwitterPoruka();
+		
+		nizTviterPoruka[0].setKorisnik("Jovana");
+		nizTviterPoruka[0].setPoruka("#cu");
+	
+		assertArrayEquals(nizTviterPoruka, t.vratiPoruke(10, "#cu"));
+	
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeTagNull() {
+		int maxBroj = 20;
+		String tag = null;
+
+		t.vratiPoruke(maxBroj, tag);
+	}
+
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeTagPrazanString() {
+		int maxBroj = 20;
+		String tag = "";
+
+		t.vratiPoruke(maxBroj, tag);
+	}
+
+	@Test
+	public void testVratiPorukeKapacitetManjiJednakNuli() {
+		int maxBroj = 0;
+		String tag = "fon";
+		
+		t.vratiPoruke(maxBroj, tag);
+	}
 }
